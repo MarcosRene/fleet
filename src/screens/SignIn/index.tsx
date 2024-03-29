41d0a9b367f326085254e21775782ac16b91769d
Marcos Rene <renemoura789@gmail.com>
2024-03-29 12:52:46 -0300

    feat: add sign in screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from 'react-native';
+import { Container, Slogan, Title } from './styles';
 
-import { styles } from './styles';
+import { Button } from '../../components/Button';
+
+import backgroundImg from '../../assets/background.png';
 
 export function SignIn() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: '#fff' }}>SignIn</Text>
-    </View>
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+
+      <Button title="Entrar com Google" />
+    </Container>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,10 +1,29 @@
-import { StyleSheet } from 'react-native';
+import styled, { css } from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const Container = styled.ImageBackground`
+  padding: 32px;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+
+  flex: 1;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.XXXL}px;
+    color: ${theme.COLORS.BRAND_LIGHT};
+    text-align: center;
+  `}
+`;
+
+export const Slogan = styled.Text`
+  ${({ theme }) => css`
+    margin-bottom: 32px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_100};
+    text-align: center;
+  `}
+`;
