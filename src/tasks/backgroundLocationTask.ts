a12ae3fa51396a164237d1dd1a195a5253961fa3
Marcos Rene <renemoura789@gmail.com>
2024-04-04 17:12:17 -0300

    feat: add tasks

---
+++ src/tasks/backgroundLocationTask.ts
@@ -0,0 +1,71 @@
+import {
+  Accuracy,
+  hasStartedLocationUpdatesAsync,
+  startLocationUpdatesAsync,
+  stopLocationUpdatesAsync,
+} from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+
+export const BACKGROUND_TASK_NAME = 'location-tracking';
+
+type TaskManagerProps = {
+  data: any;
+  error: any;
+};
+
+TaskManager.defineTask(
+  BACKGROUND_TASK_NAME,
+  ({ data, error }: TaskManagerProps) => {
+    try {
+      if (error) {
+        throw error;
+      }
+
+      const { coords, timestamp } = data.localtion[0];
+
+      const currentLocation = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        timestamp,
+      };
+
+      console.log(currentLocation);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
+
+export async function startLocationTask() {
+  try {
+    const hashStarted = await hasStartedLocationUpdatesAsync(
+      BACKGROUND_TASK_NAME
+    );
+
+    if (hashStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+    }
+
+    await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
+      accuracy: Accuracy.Highest,
+      distanceInterval: 1,
+      timeInterval: 1000,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function stopLocationTask() {
+  try {
+    const hashStarted = await hasStartedLocationUpdatesAsync(
+      BACKGROUND_TASK_NAME
+    );
+
+    if (hashStarted) {
+      await stopLocationUpdatesAsync(BACKGROUND_TASK_NAME);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
