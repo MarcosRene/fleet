ebf7acf1d533fc4eba302e620f42b12769d0ef47
Marcos Rene <renemoura789@gmail.com>
2024-04-05 14:54:54 -0300

    feat: logging the output

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -97,6 +97,13 @@ export function Departure() {
             user_id: user!.id,
             license_plate: licensePlace.toLocaleUpperCase(),
             description,
+            coords: [
+              {
+                latitude: currentCoords.latitude,
+                longitude: currentCoords.longitude,
+                timestamp: new Date().getTime(),
+              },
+            ],
           })
         );
       });
